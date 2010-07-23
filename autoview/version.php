936c0f6b965f494e4b2abac1af9270bdadf69483
Tim Williams <tmw@autotrain.org>
2010-07-23 16:20:20 +0000

    Changes for mobile device support

--- autoview/version.php
+++ autoview/version.php
@@ -5,7 +5,7 @@
 ///  This fragment is called by moodle_needs_upgrading() and /admin/index.php
 /////////////////////////////////////////////////////////////////////////////////
 
-$module->version  = 2010063001;  // The current module version (Date: YYYYMMDDXX)
+$module->version  = 2010071201;  // The current module version (Date: YYYYMMDDXX)
 $module->requires = 2007021590;  // Requires this Moodle version
 $module->cron     = 300;           // Period for cron to check this module (secs)
 
