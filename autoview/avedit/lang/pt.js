94c87d05bf743000999ac028d231b92bc17d8e52
Tim Williams <tmw@autotrain.org>
2010-08-02 12:28:02 +0000

    Import AutoView common

---
+++ autoview/avedit/lang/pt.js
@@ -0,0 +1,164 @@
+data=new Array();
+
+/*****Used by the autoview extensions*****/
+
+data["livecapture"]="Live Capture (Java)";
+data["livecaptureflash"]="Live Capture (Flash)";
+
+/*****Used by the editor*****/
+
+data["e_editmodesupport"]="Aviso : O modo de edição é somente suportado no "+
+    "Mozilla Firefox e Internet Explorer. Você pode continuar, mas pode ocorrer problemas.";
+
+data["e_use_quick_start"]="Você Gostaria de usar o  AutoView QuickStart Wizard\npara configurar sua apresentação ?";
+data["e_quick_start_title"]="AutoView QuickStart Wizard";
+
+data["e_sources"]="Fontes";
+data["e_sources_title"]="Adicionar/Editar slide e video fontes.";
+data["e_timestitles"]="Tempos/Titulos";
+data["e_timestitles_title"]="Editar titulos dos slides e linkar com o tempo.";
+data["e_subtitlesbutton"]="Subtitulos";
+data["e_subtitlesbutton_title"]="Adicionar/Editar subtitulos";
+data["e_options"]="Opções";
+data["e_options_title"]="Mostrar opções da apresentação";
+
+data["e_save"]="Salvar apresentação";
+data["e_save_title"]="Salvar a configuração no servidor. isto deve acontecer automaticamente a maior parte do tempo e, portanto, este botão não é normalmente necessário.";
+
+data["e_showthumbs"]="Mostrar miniaturas por padrão";
+data["e_showsubtitles"]="Mostrar sub-títulos por padrão";
+data["e_autostart"]="Iniciar vídeo automaticamente";
+data["e_videoright"]="Video on right";
+data["e_pauseafterslide"]="Pausa após cada slide";
+data["e_videos"]="Opções de Vídeo";
+data["e_slides"]="Opções de slides";
+data["e_subtitles"]="Opções de sub-título";
+data["e_language"]="Linguagem";
+data["e_position"]="Salvar posição";
+data["e_slideMenu"]="Slide Menu";
+data["e_thumbnail"]="Controle de miniatura";
+data["e_other"]="Outro";
+
+data["e_subtitles_message"]="Funções do subtítulo são suportadas pela plataforma subjacente AutoView 3, mas como ainda não tenham sido implementadas no editor da web."+
+  " Fazemos votos para implementar esse recurso no futuro.";
+
+data["e_Video"]="Video";
+data["e_Slide"]="Slide";
+data["e_subtitle"]="Subtítulo";
+data["e_subtitle_sources"]="Subtítulo fontes";
+data["e_Slide_sources"]="Slide fontes";
+data["e_Video_sources"]="Video fontes";
+data["e_subtitle_source"]="Subtítulo fontes";
+data["e_Slide_source"]="Slide fontes";
+data["e_Video_source"]="Video fontes";
+data["e_delete_subtitle_source"]="Deletar Subtítulo fontes";
+data["e_delete_Slide_source"]="Deletar slide fontes";
+data["e_edit_subtitle_source"]="Editar Subtítulo fontes";
+data["e_edit_Slide_source"]="Editar slide fontes";
+data["e_edit_Video_source"]="Editar video fontes";
+data["e_add_subtitle_source"]="Adcionar Subtítulo fontes";
+data["e_add_Slide_source"]="Adicionar slide fonte";
+data["e_add_Video_source"]="Adicionar video fonte";
+data["e_add_Slide_source_title"]="Adicionar um novo pre-processado slide fonte";
+data["e_add_Video_source_title"]="Adicionar um novo pre-processado video fonte";
+data["e_sources"]="Fontes";
+data["e_delete"]="Deletar";
+data["e_edit"]="Editar";
+data["e_numslides"]="Número de slides";
+data["e_add"]="Adicionar";
+data["e_source"]="Fonte";
+data["e_convert_title"]="Automaticamente converte Powerpoint ou OpenDocument apresentações em aptos slides";
+data["e_convert_button"]="Converter Slides";
+data["e_livecapture_title"]="Usar o  AutoView Live Capture Plugin para gravar um video para usar com esta apresentação";
+data["e_livecapture_button"]="Gravar Video";
+data["e_flashreplace"]="Você já tem um vídeo em Flash em sua apresentação.\nVocê gostaria de substituir esse vídeo com a gravação nova ?";
+data["e_alreadylive"]="Um live capture source foi adicionada. Você não pode ter duas.";
+data["e_livecapturechoice"]="Você gostaria de usar o Java Applet para ter uma alta qaulidade de captura ?";
+data["e_set"]="Setar";
+data["e_bandwidth"]="Bandwidth";
+data["e_video_url"]="Video URL";
+data["e_event_triggers"]="Event triggers ?";
+data["e_slide_url"]="Slide URL";
+data["e_image_type"]="Tipo de Imagem";
+data["e_convert_slides"]="Converter Slides";
+data["e_detect_video_type"]="Detectar tipo de vídeo";
+data["e_detect_video_type_title"]="Determinar automaticamente os melhores plugins de vídeos para usar com o vídeo selecionado";
+data["e_unrecognised_video_type"]="Tipo de vídeo não reconhecido";
+data["e_video_type_warn"]="Aviso, P player de vídeo selecionado pode não ser capaz \nde reproduzir o arquivo de vídeo seleccionado.\nDeseja continuar ?";
+data["e_videopos"]="Posição do vídeo";
+
+data["e_set_title"]="Setar Slide Titulo";
+data["e_set_times"]="Setar Trigger Time";
+data["e_set_time_button"]="Setar Tempo";
+data["e_moveset_button"]="Mover & Setar";
+data["e_set_slidetime_title"]="Setar o trigger time do corrente slide";
+data["e_slidemoveset_title"]="Mover para o proximo slide e setar para o trigger time";
+data["e_set_subtime_title"]="Setar o trigger time do corrente subtitulo";
+data["e_submoveset_title"]="Mover para o corrente subtitulo e setar o trigger time";
+data["e_slide_plural"]="Slides";
+data["e_sub_plural"]="Subtitulos";
+data["e_cleartimes_button"]="Deletar Tempos";
+data["e_cleartimes_title"]="Deletar todos os tempos";
+data["e_clearslidetimes"]="Você esta certo que quer deletar todos os tempos dos slides ?";
+data["e_clearsubtimes"]="Você esta certo que quer deletar todos os tempos dos subtitulos ?";
+
+data["e_new_sub_source"]="Adicionar subtitulo fonte";
+data["e_sub_not_active"]="Subtitulo precisa estar ligado antes de você poder editar conteúdo.";
+data["e_subtitle_url"]="Subtitulo arquivo URL";
+data["e_subtitle_source_type"]="Subtitulo fonte tipo";
+data["e_subtype_jsavsfile"]="AutoView Subtitulos arquivo (.avs or .js file extension)";
+data["e_subtype_blank"]="Criar arquivo de subtitulo em branco$";
+data["e_subtype_textfile"]="Importar subtitulo de um arquivo de texto (.txt file extension)";
+data["e_subtype_textbox"]="Importar subtitulo de uma caixa de texto (recortar e colar)";
+data["e_subtitle_boxname"]="Trascrição de subtitulo";
+data["e_sub_split"]="Esta transcrição contém legendas muito longas, você quer dividir-las automaticamente ?";
+data["e_subediting"]="Editar Subtitulos";
+data["e_addsubbeforetitle"]="Adicionar um novo subtitulo antes do corrente subtitulo";
+data["e_addsubbefore"]="Adicionar depois";
+data["e_addsubaftertitle"]="Adicionar um novo subtitulo depois do corrente subtitulo";
+data["e_addsubafter"]="Adicionar antes";
+data["e_editsubtitle"]="Editar o subtitulo";
+data["e_editsub"]="Editar subtitulo";
+data["e_deletesubtitle"]="Deletar o corrente subtitulo";
+data["e_deletesub"]="Deletar este subtitulo";
+data["e_subtitlesummary"]="Indice subtítulo :";
+data["e_subtitlefileformat"]="Suas legendas estão atualmente armazenados como um arquivo JavaScript, que não pode ser editadas.\n Se você quer salvar sues subtitulos  como um arquivo de subtitulo do Autoview,\nclick OK, ou entao clique em Cancelar.";
+
+data["e_slide_time_warn_prev"]="Tempo do slide deve ser antes do tempo do(s) slide(s) anterior(es)";
+data["e_slide_time_warn_next"]="Tempo do slide deve ser depois do tempo do(s) slide(s) anterior(es)";
+data["e_sub_time_warn_prev"]="Tempo do subtitulo deve ser antes do tempo do(s) subtitulo(s) anterior(es)";
+data["e_sub_time_warn_next"]="Tempo do subtitulo deve ser depois do tempo do(s) subtitulo(s) anterior(es)";
+
+data["e_delete_source"]="Você deseja mesmo deletar este fonte?";
+
+data["e_none"]="Não";
+data["e_eventnote"]="Esta definição só é necessária se você tiver feito tratamento especial para adicionar evento dispara para o vídeo. Se você não entender o que isto significa, não mude isso";
+data["e_update"]="Atualizar";
+
+data["e_print_only"]="Somente para impressão";
+data["e_image_only"]="Somente usado para imagens baseadas em slides";
+data["e_openoffice"]="OpenOffice Convertor";
+data["e_avconvert"]="AutoView Convertor";
+
+data["e_convert_heading"]="Arquivo de slide convertido";
+data["e_slide_file"]="Arquivo de slide";
+data["e_convert_to"]="Converter para";
+data["e_select"]="Selecionar";
+data["e_convert"]="Converter";
+
+data["e_convertmessage"]="O serviço de conversão do documento nao esta configurado no servidor. Por favor visite http://autoview.autotrain.org para maiores informações.";
+
+data["e_flash"]="Adobe Flash";
+data["e_pdf"]="Adobe PDF";
+data["e_images"]="JPEG Imagem";
+
+data["e_nolivecapture"]="The live capture applet não esta instalado no seu sistema. Por favor visite o AutoTrain AutoView website para maiores informações.";
+data["e_nolivecapture2"]="AutoView Website";
+data["e_noflashserver"]="The Flash Server não esta configurado para este modulo. Isso é necessario para usar o  LiveCapture with Flash.";
+data["e_noflashcapture"]="The Flash capture URL não esta configurado para este modulo. Por favor visite http://autoview.autotrain.org para maiores informações.";
+
+data["e_streamquestion"]="Você deseja usar streaming para melhorar o desempenho da reprodução de vídeo ?";
+
+/*****Merge the additional lang strings*****/
+
+mergeLangStrings("pt", data);
