3df8d28970fcad072a0467a7a28380dca0036070
Tim Williams <tmw@autotrain.org>
2010-11-01 17:14:21 +0000

    Added curl support to the document converter code

--- convert.php
+++ convert.php
@@ -3,6 +3,17 @@
 * This code is copyright to EuroMotor AutoTrain LLP and is licenced under the GNU GPLv2 Licence
 **/
 
+
+ register_shutdown_function('fatalErrorShutdownHandler');
+ 
+ function fatalErrorShutdownHandler()
+ {
+  $last_error = error_get_last();
+  if ($last_error['type'] === E_ERROR) {
+    echo "<pre>Fatal error:\n\n".$last_error['message']."\n".$last_error['file']."\n".$last_error['line']."</pre>";
+  }
+ }
+
  require_once("../../config.php");
  require_once("lib.php");
 
@@ -28,23 +39,21 @@
 
  if (!has_capability('mod/autoview:canconvertdocument', $context))
  {
-   die(get_string("convertnotallowed", "autoview"));
+  die(get_string("convertnotallowed", "autoview"));
  }
 
  /***Check this server is capable of commuinicating with the convertor***/
- if (testPeclHttp()==false)
+ if (testDocConv()==false)
  {
   die(get_string("pecl_http_warn", "autoview"));
  }
 
- $convertdata=convertPresentationFile($swf, $pdf, $jpg, $CFG->autoview_conversionkey,
+ $convertfile=convertPresentationFile($swf, $pdf, $jpg, $CFG->autoview_conversionkey,
   $CFG->autoview_conversionurl, $CFG->dataroot.'/'.$course->id.'/'.$url);
 
  $aveditdir=$CFG->wwwroot.'/mod/autoview/avedit/';
 
- //Check we got something back
-
- if (!$convertdata)
+ if ($convertfile==false)
  {
   $params= array(
    'aveditdir' => $aveditdir,
@@ -59,12 +68,12 @@
   echo process_xsl($CFG->dirroot."/mod/autoview/avedit/convert.xml", $CFG->dirroot."/mod/autoview/avedit/convert.xsl", $params);
  }
  else
- if ($convertdata->headers["Content-Type"]=="text/plain")
+ if (strpos($convertfile, ".txt")>0)
  {
   $params= array(
    'aveditdir' => $aveditdir,
    'message' => get_string("conversionfailed", "autoview"),
-   'message2' => $convertdata->body,
+   'message2' => file_get_contents($convertfile),
    'fname' => '',
    'error' => 'true',
    'buttontext' => get_string("closetext", "autoview"),
@@ -73,23 +82,18 @@
    'pdf' => '',
    'jpg' => '');
   echo process_xsl($CFG->dirroot."/mod/autoview/avedit/convert.xml", $CFG->dirroot."/mod/autoview/avedit/convert.xsl", $params);
+  unlink($convertfile);
  }
  else
+ if (strpos($convertfile, ".xml")>0)
  {
-  $fname=stripFileExtension($url);
-  $newfile=$CFG->dataroot.'/'.$course->id.'/'.$fname.'.zip';
-
-  extactConvertorZip($convertdata, $newfile);
-  //die(get_string("convertsavefailed", "autoview"));
-
-  $xmlFile=$CFG->dataroot.'/'.$course->id.'/'.$fname.'.xml';
-  if (!file_exists($xmlFile))
+  if (!file_exists($convertfile))
   {
    $params=array(
     'aveditdir' => $aveditdir,
     'message' => get_string("conversionfailed", "autoview"),
     'message2' => get_string("conversionfailed_noerror", "autoview"),
-    'fname' => $fname,
+    'fname' => '',
     'error' => 'true',
     'buttontext' => get_string("closetext", "autoview"),
     'swf' => $swf,
@@ -103,15 +107,15 @@
    $params=array(
     'aveditdir' => $aveditdir,
     'message' => get_string("conversiondone", "autoview"),
-    'fname' => $fname,
+    'fname' => stripFileExtension($url),
     'error' => 'false',
     'buttontext' => get_string("addtext", "autoview"),
     'swf' => $swf,
     'pdf' => $pdf,
     'jpg' => $jpg);
 
-   echo process_xsl($xmlFile, $CFG->dirroot."/mod/autoview/avedit/convert.xsl", $params);
-   unlink($xmlFile);
+   echo process_xsl($convertfile, $CFG->dirroot."/mod/autoview/avedit/convert.xsl", $params);
+   unlink($convertfile);
   }
  }
 ?>
