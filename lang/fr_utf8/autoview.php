360f02aace1254f16ff13cc0d52948303da5f7cc
Tim Williams <tmw@autotrain.org>
2011-04-21 15:59:41 +0000

    Adding to 2011042101 release

---
+++ lang/fr_utf8/autoview.php
@@ -0,0 +1,34 @@
+<?PHP // $Id: label.php,v 1.2 2003/11/18 07:28:46 moodler Exp $ 
+      // start.php - created with Moodle 1.2 development (2003111400)
+
+$string['autoviewtext'] = 'Nom de la présentation AutoView ';
+$string['chooseconfig'] = 'Choisir un fichier de configuration';
+$string['configfile'] = 'Fichier de configuration';
+$string['convertsavefailed'] = 'Impossible de sauvegarder le fichier converti';
+$string['editbutton'] = 'Activer l\'édition';
+$string['editoff'] = 'Désactiver l\'édition';
+$string['hidenav'] = 'Cacher la navigation';
+$string['modulename'] = 'Présentation AutoView';
+$string['modulenameplural'] = 'Présentations AutoView';
+$string['xmlsavefailed'] = 'Echec de sauvegarde de la configuration';
+$string['configconversionurl'] = 'URL du service de conversion de document';
+$string['configconversionkey'] = 'Clef d\'accès au service de conversion de document';
+$string['conversiondone'] = 'Le document a été converti.';
+$string['conversionfailed'] = 'Echec de la conversion';
+$string['xmlhelp']= 'Dans la plupart des cas le fichier de configuration par défaut conviendra. Il est directement créé à la racine des fichiers du cours '.
+ 'à partir du nom de la ressource AutoView. Cependant, si vous avez copié une présentation AutoView à partir d\'un autre'.
+ ' décochez la case et copiez le nom du fichier .avx de configuration de la présentation ici. Vous pouvez également décocher la case et '.
+ ' utiliser un fichier d\'extension .avx si vous voulez pointer manuellement un fichier de configuration AutoView.';
+$string['xmlnote'] = 'Note : En choisissant un nom de fichier de configuration non encore utilisé, vous créerez un nouveau fichier de configuration.';
+$string['noxsl'] = 'Attention : Ce système ne dispose pas de parseur XSL valide. Mettez à jour votre version de PHP en activant le support XSL. Voir le fichier LisezMoi du module AutoView pour plus d\'information sur comment le faire';
+$string['pecl_http_warn'] = 'Attention : L\'extension pecl_http PHP n\'est pas disponible sur votre serveur. Vous en avez besoin pour utiliser les services de conversion de document. Ce module peut être téléchargé depuis http://pecl.php.net/package/pecl_http/. Si vous n\'utilisez aucun service de conversion de document, cet avertissement peut être ignoré.';
+$string['createnew'] = 'Utiliser la configuration AutoView par défaut (recomandé)';
+$string['configlivecapture']= 'URL de l\'applet de capture';
+$string['configflashserver']= 'URL du serveur de communication Flash/Red5';
+$string['confignote'] = 'Ces paramètres servent à configurer';
+$string['confignote2'] = 'les services additionnels';
+$string['confignote3'] = 'pour AutoView. Laissez les non renseignez si vous n\'utilisez pas de sevices additionnels.';
+$string['confignote4'] = 'Voir le site AutoView pour plus de détail.';
+$string['waitermessage'] = 'Veuillez patienter : Si l\'interface de l\'éditeur n\'apparait pas lorsque le cadre de droite a terminé son chargement, cliquez sur le bouton ci-dessous';
+$string['starteditor'] = 'Démarrer l\'éditeur';
+?>
