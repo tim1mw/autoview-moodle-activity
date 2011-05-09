8042f21a4f51558e7efc2ec71a50fc7416888013
Tim Williams <tmw@autotrain.org>
2011-05-09 16:18:58 +0000

    Version updated to 2011050901

--- version.php
+++ version.php
@@ -5,7 +5,7 @@
 ///  This fragment is called by moodle_needs_upgrading() and /admin/index.php
 /////////////////////////////////////////////////////////////////////////////////
 
-$module->version  = 2011042101;  // The current module version (Date: YYYYMMDDXX)
+$module->version  = 2011050901;  // The current module version (Date: YYYYMMDDXX)
 $module->requires = 2007021590;  // Requires this Moodle version
 $module->cron     = 300;           // Period for cron to check this module (secs)
 
