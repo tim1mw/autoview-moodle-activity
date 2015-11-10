fd8e44af11cd492a8c537e0b59edce3e648021f0
Tim Williams <tmw@autotrain.org>
2015-11-10 11:01:40 +0000

    Change $module to $plugin for Moodle 3.x compat

--- autoview/version.php
+++ autoview/version.php
@@ -1,4 +1,4 @@
-<?PHP
+<?php
 
 /////////////////////////////////////////////////////////////////////////////////
 ///  Code fragment to define the version of start
@@ -7,13 +7,13 @@
 
 global $CFG;
 
-$module->version  = 2015080503;  // The current module version (Date: YYYYMMDDXX)
-$module->requires = 2013111800;
-$module->cron     = 300;           // Period for cron to check this module (secs)
+$plugin->version  = 2015111001;  // The current module version (Date: YYYYMMDDXX)
+$plugin->requires = 2013111800;
+$plugin->cron     = 300;           // Period for cron to check this module (secs)
 
-$module->release = $module->version;
-$module->maturity = MATURITY_STABLE;
+$plugin->release = $module->version;
+$plugin->maturity = MATURITY_STABLE;
 
-$module->component = 'mod_autoview';
+$plugin->component = 'mod_autoview';
 
 ?>
