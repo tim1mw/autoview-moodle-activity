e12973ace4e32e4dc0ea140dbcedb5356396d91a
Tim Williams <tmw@autotrain.org>
2015-12-01 13:25:57 +0000

    Fix warning in version file.

--- autoview/version.php
+++ autoview/version.php
@@ -5,13 +5,11 @@
 ///  This fragment is called by moodle_needs_upgrading() and /admin/index.php
 /////////////////////////////////////////////////////////////////////////////////
 
-global $CFG;
-
 $plugin->version  = 2015111001;  // The current module version (Date: YYYYMMDDXX)
 $plugin->requires = 2013111800;
 $plugin->cron     = 300;           // Period for cron to check this module (secs)
 
-$plugin->release = $module->version;
+$plugin->release = $plugin->version;
 $plugin->maturity = MATURITY_STABLE;
 
 $plugin->component = 'mod_autoview';
