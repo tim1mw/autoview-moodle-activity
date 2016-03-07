2c014830a0b46e8b12844dfbe2546213c17fae57
Tim Williams <tmw@autotrain.org>
2016-03-07 15:53:13 +0000

    Set version 2016030701 and BETA since there have been a lot of changes since the last release.

--- autoview/version.php
+++ autoview/version.php
@@ -5,12 +5,12 @@
 ///  This fragment is called by moodle_needs_upgrading() and /admin/index.php
 /////////////////////////////////////////////////////////////////////////////////
 
-$plugin->version  = 2015111001;  // The current module version (Date: YYYYMMDDXX)
+$plugin->version  = 2016030701;  // The current module version (Date: YYYYMMDDXX)
 $plugin->requires = 2013111800;
 $plugin->cron     = 300;           // Period for cron to check this module (secs)
 
 $plugin->release = $plugin->version;
-$plugin->maturity = MATURITY_STABLE;
+$plugin->maturity = MATURITY_BETA;
 
 $plugin->component = 'mod_autoview';
 
