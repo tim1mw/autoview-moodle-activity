53fd5cc101fa21dbf4c024423dd1ca7d1cb3137a
Tim Williams <tmw@autotrain.org>
2018-05-31 17:12:50 +0100

    Set stable maturity and update version

--- version.php
+++ version.php
@@ -5,12 +5,12 @@
 ///  This fragment is called by moodle_needs_upgrading() and /admin/index.php
 /////////////////////////////////////////////////////////////////////////////////
 
-$plugin->version  = 2018050202;  // The current module version (Date: YYYYMMDDXX)
+$plugin->version  = 2018053101;  // The current module version (Date: YYYYMMDDXX)
 $plugin->requires = 2018042700;
 $plugin->cron     = 300;           // Period for cron to check this module (secs)
 
 $plugin->release = $plugin->version;
-$plugin->maturity = MATURITY_BETA;
+$plugin->maturity = MATURITY_STABLE;
 
 $plugin->component = 'mod_autoview';
 
