1bf8f24fa092758528b77eb719b589255be292b8
Tim Williams <tmw@autotrain.org>
2018-05-31 17:25:58 +0100

    Add block_repo_filemanager and repository_coursefilearea dependencies and update version number

--- version.php
+++ version.php
@@ -5,7 +5,7 @@
 ///  This fragment is called by moodle_needs_upgrading() and /admin/index.php
 /////////////////////////////////////////////////////////////////////////////////
 
-$plugin->version  = 2018053101;  // The current module version (Date: YYYYMMDDXX)
+$plugin->version  = 2018053102;  // The current module version (Date: YYYYMMDDXX)
 $plugin->requires = 2018042700;
 $plugin->cron     = 300;           // Period for cron to check this module (secs)
 
@@ -13,5 +13,10 @@ $plugin->release = $plugin->version;
 $plugin->maturity = MATURITY_STABLE;
 
 $plugin->component = 'mod_autoview';
+
+$plugin->dependencies = array(
+    'block_repo_filemanager' => 2018053101,
+    'repository_coursefilearea' => 2018053101
+);
 
 ?>
